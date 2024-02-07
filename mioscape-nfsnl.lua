b96d5cb9c0283d519f3f2f81c09bd969b3e0208a
ihya.dev <git@ihya.dev>
2024-02-08 04:03:07 +0700

    feat: level

--- mioscape-nfsnl.lua
+++ mioscape-nfsnl.lua
@@ -5,11 +5,13 @@ function START()
   MENU=gg.choice({
     '\n[Money]\n',
     '\n[Gold]\n',
+    '\n[Level]\n',
     '\n[Exit]\n',
 }, nil, '[NFS:NL v0.4beta]')
 if MENU == 1 then Money() end
 if MENU == 2 then Gold() end
-if MENU == 3 then Exit() end
+if MENU == 3 then Level() end
+if MENU == 4 then Exit() end
 MIO=-1
 end
 
@@ -35,6 +37,18 @@ function Gold()
   gg.clearResults()
   gg.processResume()
   gg.toast('Gold Changed')
+end
+
+function Level()
+    gg.processPause()
+    mio = gg.prompt({[1]='Change to:', [2]='Current Level:'}, {[1]='', [2]=''})
+    gg.searchNumber(mio[2]..';327700~327720::13', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+    gg.refineNumber(mio[2], gg.TYPE_DWORD)
+    gg.getResult(100)
+    gg.editAll(mio[1], gg.TYPE_DWORD)
+    gg.clearResults()
+    gg.processResume()
+    gg.toast('Level Changed')
 end
 
 function Exit()
